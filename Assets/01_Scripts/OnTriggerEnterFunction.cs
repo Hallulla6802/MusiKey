7f54818cee9ae1d488dd254252abca3f7663aa93
CdeCejota <cesar.jaraf@uniacc.edu>
2024-09-18 00:03:47 -0300

    Spawn "Z" & "X" Notes and destroy it when key code donws

--- Assets/01_Scripts/OnTriggerEnterFunction.cs
+++ Assets/01_Scripts/OnTriggerEnterFunction.cs
@@ -6,7 +6,12 @@ public class OnTriggerEnterFunction : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Sprite"))
+        if(other.gameObject.CompareTag("SpriteX") && Input.GetKeyDown(KeyCode.X))
+        {
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("SpriteZ") && Input.GetKeyDown(KeyCode.Z))
         {
             Destroy(other.gameObject);
         }
--- Assets/01_Scripts/SpawnersScript.cs
+++ Assets/01_Scripts/SpawnersScript.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnersScript : MonoBehaviour
 {
-    public GameObject spritePrefab;        // The sprite to spawn
+    public GameObject spriteZ;   // the sprite Z to spawn 
+    public GameObject spriteX;   // The sprite X to spawn
     public Transform[] spawners;           // Array of spawner positions (empty GameObjects)
     public KeyCode[] spawnKeys;            // Array of keys to trigger each spawner
     public float speed = 5f;               // Speed of movement
-    public string spriteTag = "Sprite";    // Tag for identifying sprites
+    public string spritexTag = "SpriteX";
+    public string spritezTag = "SpriteZ"; 
+    public int randomNumber;
     
 
     private void Update()
@@ -30,21 +34,39 @@ public class SpawnersScript : MonoBehaviour
     
     void SpawnSpriteAtSpawner(int index)
     {
-        GameObject newSprite = Instantiate(spritePrefab, spawners[index].position, Quaternion.identity);
-        newSprite.tag = spriteTag; 
-        
+        randomNumber = Random.Range(0, 2);
+        if (randomNumber == 0)
+        {
+            GameObject newSprite = Instantiate(spriteX, spawners[index].position, Quaternion.identity);
+            newSprite.tag = spritexTag;
+        }
+
+        if (randomNumber == 1)
+        {
+            GameObject newSprite = Instantiate(spriteZ, spawners[index].position, Quaternion.identity);
+            newSprite.tag = spritezTag;
+        }
+
     }
 
     
     void MoveSprites()
     {
-        GameObject[] sprites = GameObject.FindGameObjectsWithTag(spriteTag);
-        foreach (GameObject sprite in sprites)
+        
+        GameObject[] spritexs = GameObject.FindGameObjectsWithTag(spritexTag);
+        foreach (GameObject sprite in spritexs)
         {
-            sprite.transform.Translate(Vector3.back * speed * Time.deltaTime);
+            sprite.transform.Translate(Vector3.back * speed * Time.deltaTime);  
+        }
+
+        
+        GameObject[] spritezs = GameObject.FindGameObjectsWithTag(spritezTag);
+        foreach (GameObject sprite in spritezs)
+        {
+            sprite.transform.Translate(Vector3.back * speed * Time.deltaTime);  
         }
     }
 
-    
-    
+
+
 }
