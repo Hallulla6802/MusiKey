45d951f1e912fa8a0e0f2c523900f0d7745a76d0
TheWolfHope <esperanzaycopito@gmail.com>
2024-09-24 17:35:50 -0300

    combos sytem fixed

--- Assets/01_Scripts/ComboCount.cs
+++ Assets/01_Scripts/ComboCount.cs
@@ -7,6 +7,8 @@ public class ComboCount : MonoBehaviour
     private GameManager gamMan;
     private UpdateUIManager upUI;
 
+    private HashSet<Collider> triggeredSprites = new HashSet<Collider>();  // Track sprites that have triggered the combo
+
     private void Awake()
     {
         gamMan = FindObjectOfType<GameManager>();
@@ -15,22 +17,43 @@ public class ComboCount : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if ((other.gameObject.CompareTag("SpriteX") && Input.GetKeyDown(KeyCode.X)) || (other.gameObject.CompareTag("SpriteZ") && Input.GetKeyDown(KeyCode.Z)))
+        if (!triggeredSprites.Contains(other) && IsCorrectKeyInput(other))
         {
-            gamMan.combos++;
-            gamMan.UpdateCombos();
-            upUI.UpdateUIText();
-
+            AddCombo(other);
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if ((other.gameObject.CompareTag("SpriteX") && Input.GetKeyDown(KeyCode.X)) || (other.gameObject.CompareTag("SpriteZ") && Input.GetKeyDown(KeyCode.Z)))
+        if (!triggeredSprites.Contains(other) && IsCorrectKeyInput(other))
         {
-            gamMan.combos++;
-            gamMan.UpdateCombos();
-            upUI.UpdateUIText();
+            AddCombo(other);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Remove the sprite from the HashSet so the combo can be triggered again for the next sprite
+        if (triggeredSprites.Contains(other))
+        {
+            triggeredSprites.Remove(other);
+        }
+    }
+
+    private bool IsCorrectKeyInput(Collider other)
+    {
+        // Check for the correct key input based on the tag of the sprite
+        return (other.gameObject.CompareTag("SpriteX") && Input.GetKeyDown(KeyCode.X)) ||
+               (other.gameObject.CompareTag("SpriteZ") && Input.GetKeyDown(KeyCode.Z));
+    }
+
+    private void AddCombo(Collider other)
+    {
+        gamMan.combos++;
+        gamMan.UpdateCombos();
+        upUI.UpdateUIText();
+
+        // Add the sprite to the HashSet to prevent it from triggering multiple times
+        triggeredSprites.Add(other);
     }
 }
